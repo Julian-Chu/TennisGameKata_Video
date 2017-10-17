35fa65d80c5fe93e4bb70b347c332570c6ad79e0
hatelove <jay.hatelove@gmail.com>
2017-10-17 15:28:26 +0800

    add failed test case: deuce when 4:4

--- TennisGameKata_Video/UnitTest1.cs
+++ TennisGameKata_Video/UnitTest1.cs
@@ -72,6 +72,13 @@ namespace TennisGameKata_Video
             ScoreShouldBe("Deuce");
         }
 
+        [TestMethod]
+        public void Deuce_When_4_4()
+        {
+            GivenFirstPlayerScoreTimes(4);
+            GivenSecondPlayerScoreTimes(4);
+            ScoreShouldBe("Deuce");
+        }
         private void GivenSecondPlayerScoreTimes(int times)
         {
             for (int i = 0; i < times; i++)
