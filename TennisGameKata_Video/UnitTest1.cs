e42830489bb3304110aaf1b0d813abe2aa759ab3
hatelove <jay.hatelove@gmail.com>
2017-10-17 15:11:34 +0800

    refactor testing: extract method ScoreShouldBe()

--- TennisGameKata_Video/UnitTest1.cs
+++ TennisGameKata_Video/UnitTest1.cs
@@ -5,12 +5,18 @@ namespace TennisGameKata_Video
     [TestClass]
     public class TennisGameTests
     {
+        private TennisGame tennisGame = new TennisGame();
+
         [TestMethod]
         public void love_all()
         {
-            TennisGame tennisGame = new TennisGame();
+            ScoreShouldBe("Love All");
+        }
+
+        private void ScoreShouldBe(string expected)
+        {
             string score = tennisGame.Score();
-            Assert.AreEqual("Love All",score);
+            Assert.AreEqual(expected, score);
         }
     }
 }
