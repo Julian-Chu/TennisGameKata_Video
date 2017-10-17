2ae9ae5f60a0734783a673bcc3f6cb4b00cad7c7
hatelove <jay.hatelove@gmail.com>
2017-10-17 15:36:08 +0800

    add failed test case: first player win when 4:0

--- TennisGameKata_Video/UnitTest1.cs
+++ TennisGameKata_Video/UnitTest1.cs
@@ -96,6 +96,13 @@ namespace TennisGameKata_Video
             ScoreShouldBe("Mei Adv");
         }
 
+        [TestMethod]
+        public void FirstPlayer_Win_When_4_0()
+        {
+            GivenFirstPlayerScoreTimes(4);
+            ScoreShouldBe("Joey Win");
+        }
+
         private void GivenSecondPlayerScoreTimes(int times)
         {
             for (int i = 0; i < times; i++)
